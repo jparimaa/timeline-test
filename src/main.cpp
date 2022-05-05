9259d8e459e1f4e6059d7fdb34c7b0a168676f61
JP Arimaa <juhapekka.arimaa@gmail.com>
2022-05-05 10:47:23 +0300

    Add test code

---
+++ src/main.cpp
@@ -0,0 +1,264 @@
+#include <cstdio>
+#include <vector>
+#include <vulkan/vulkan.h>
+
+VkInstance m_instance;
+VkDebugUtilsMessengerEXT m_debugMessenger;
+VkPhysicalDevice m_physicalDevice;
+int m_graphicsFamilyIndex = -1;
+VkDevice m_device;
+VkQueue m_graphicsQueue;
+VkCommandPool m_graphicsCommandPool;
+VkCommandBuffer m_commandBuffer;
+VkSemaphore m_timelineSemaphore;
+
+const std::vector<const char*> c_validationLayers = {"VK_LAYER_KHRONOS_validation"};
+const std::vector<const char*> c_instanceExtensions = {VK_EXT_DEBUG_UTILS_EXTENSION_NAME};
+const std::vector<const char*> c_deviceExtensions = {VK_KHR_TIMELINE_SEMAPHORE_EXTENSION_NAME};
+
+#define VK_CHECK(f)                                                                             \
+    do                                                                                          \
+    {                                                                                           \
+        const VkResult result = (f);                                                            \
+        if (result != VK_SUCCESS)                                                               \
+        {                                                                                       \
+            printf("Abort. %s failed at %s:%d. Result = %d\n", #f, __FILE__, __LINE__, result); \
+            abort();                                                                            \
+        }                                                                                       \
+    } while (false)
+
+#define CHECK(f)                                                           \
+    do                                                                     \
+    {                                                                      \
+        if (!(f))                                                          \
+        {                                                                  \
+            printf("Abort. %s failed at %s:%d\n", #f, __FILE__, __LINE__); \
+            abort();                                                       \
+        }                                                                  \
+    } while (false)
+
+template<typename T>
+uint32_t ui32Size(const T& container)
+{
+    return static_cast<uint32_t>(container.size());
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL debugUtilsCallback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+                                                  VkDebugUtilsMessageTypeFlagsEXT message_type,
+                                                  const VkDebugUtilsMessengerCallbackDataEXT* callback_data,
+                                                  void* user_data)
+{
+    if (message_severity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+    {
+        printf("Vulkan warning ");
+    }
+    else if (message_severity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT)
+    {
+        printf("Vulkan error ");
+    }
+    else
+    {
+        return VK_FALSE;
+    }
+
+    printf("(%d)\n%s\n%s\n\n", callback_data->messageIdNumber, callback_data->pMessageIdName, callback_data->pMessage);
+    return VK_FALSE;
+}
+
+void createInstance()
+{
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "MyApp";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_2;
+
+    VkDebugUtilsMessengerCreateInfoEXT debugUtilsCreateInfo{};
+    debugUtilsCreateInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    debugUtilsCreateInfo.messageSeverity = //
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT | //
+        VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT;
+    debugUtilsCreateInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT;
+    debugUtilsCreateInfo.pfnUserCallback = debugUtilsCallback;
+
+    VkInstanceCreateInfo instanceCreateInfo{};
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pApplicationInfo = &appInfo;
+    instanceCreateInfo.enabledExtensionCount = ui32Size(c_instanceExtensions);
+    instanceCreateInfo.ppEnabledExtensionNames = c_instanceExtensions.data();
+    instanceCreateInfo.enabledLayerCount = ui32Size(c_validationLayers);
+    instanceCreateInfo.ppEnabledLayerNames = c_validationLayers.data();
+    instanceCreateInfo.pNext = &debugUtilsCreateInfo;
+
+    VK_CHECK(vkCreateInstance(&instanceCreateInfo, nullptr, &m_instance));
+
+    auto vkCreateDebugUtilsMessengerEXT = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(m_instance, "vkCreateDebugUtilsMessengerEXT");
+    CHECK(vkCreateDebugUtilsMessengerEXT);
+    VK_CHECK(vkCreateDebugUtilsMessengerEXT(m_instance, &debugUtilsCreateInfo, nullptr, &m_debugMessenger));
+}
+
+void getPhysicalDevice()
+{
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(m_instance, &deviceCount, nullptr);
+    CHECK(deviceCount);
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(m_instance, &deviceCount, devices.data());
+    m_physicalDevice = devices[0];
+    CHECK(m_physicalDevice != VK_NULL_HANDLE);
+
+    VkPhysicalDeviceProperties m_physicalDeviceProperties;
+    vkGetPhysicalDeviceProperties(m_physicalDevice, &m_physicalDeviceProperties);
+    printf("GPU: %s\n", m_physicalDeviceProperties.deviceName);
+}
+
+void createDevice()
+{
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(m_physicalDevice, &queueFamilyCount, nullptr);
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(m_physicalDevice, &queueFamilyCount, queueFamilies.data());
+
+    for (unsigned int i = 0; i < queueFamilies.size(); ++i)
+    {
+        if (queueFamilies[i].queueCount > 0 && queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
+        {
+            m_graphicsFamilyIndex = i;
+        }
+    }
+
+    CHECK(m_graphicsFamilyIndex >= 0);
+
+    std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+    const float queuePriority = 1.0f;
+
+    VkDeviceQueueCreateInfo queueCreateInfo{};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = m_graphicsFamilyIndex;
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+    queueCreateInfos.push_back(queueCreateInfo);
+
+    VkPhysicalDeviceFeatures deviceFeatures{};
+    VkPhysicalDeviceVulkan12Features device12Features{};
+    device12Features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES;
+    device12Features.timelineSemaphore = true;
+
+    VkDeviceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    createInfo.pNext = &device12Features;
+    createInfo.queueCreateInfoCount = ui32Size(queueCreateInfos);
+    createInfo.pQueueCreateInfos = queueCreateInfos.data();
+    createInfo.pEnabledFeatures = &deviceFeatures;
+    createInfo.enabledExtensionCount = ui32Size(c_deviceExtensions);
+    createInfo.ppEnabledExtensionNames = c_deviceExtensions.data();
+    createInfo.enabledLayerCount = ui32Size(c_validationLayers);
+    createInfo.ppEnabledLayerNames = c_validationLayers.data();
+
+    VK_CHECK(vkCreateDevice(m_physicalDevice, &createInfo, nullptr, &m_device));
+
+    vkGetDeviceQueue(m_device, m_graphicsFamilyIndex, 0, &m_graphicsQueue);
+}
+
+void createCommandPool()
+{
+    VkCommandPoolCreateInfo poolInfo{};
+    poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    poolInfo.queueFamilyIndex = m_graphicsFamilyIndex;
+    poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+
+    VK_CHECK(vkCreateCommandPool(m_device, &poolInfo, nullptr, &m_graphicsCommandPool));
+}
+
+void allocateCommandBuffer()
+{
+    VkCommandBufferAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocInfo.commandPool = m_graphicsCommandPool;
+    allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocInfo.commandBufferCount = 1;
+
+    VK_CHECK(vkAllocateCommandBuffers(m_device, &allocInfo, &m_commandBuffer));
+}
+
+void createTimelineSemaphore()
+{
+    VkSemaphoreTypeCreateInfo timelineCreateInfo{};
+    timelineCreateInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_TYPE_CREATE_INFO;
+    timelineCreateInfo.pNext = NULL;
+    timelineCreateInfo.semaphoreType = VK_SEMAPHORE_TYPE_TIMELINE;
+    timelineCreateInfo.initialValue = 0;
+
+    VkSemaphoreCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+    createInfo.pNext = &timelineCreateInfo;
+    createInfo.flags = 0;
+
+    VK_CHECK(vkCreateSemaphore(m_device, &createInfo, NULL, &m_timelineSemaphore));
+}
+
+void destroyResources()
+{
+    vkDeviceWaitIdle(m_device);
+    vkDestroySemaphore(m_device, m_timelineSemaphore, nullptr);
+    vkDestroyCommandPool(m_device, m_graphicsCommandPool, nullptr);
+    vkDestroyDevice(m_device, nullptr);
+    auto vkDestroyDebugUtilsMessengerEXT = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(m_instance, "vkDestroyDebugUtilsMessengerEXT");
+    CHECK(vkDestroyDebugUtilsMessengerEXT);
+    vkDestroyDebugUtilsMessengerEXT(m_instance, m_debugMessenger, nullptr);
+    vkDestroyInstance(m_instance, nullptr);
+}
+
+int main(void)
+{
+    createInstance();
+    getPhysicalDevice();
+    createDevice();
+    createCommandPool();
+    allocateCommandBuffer();
+    createTimelineSemaphore();
+
+    {
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_SIMULTANEOUS_USE_BIT;
+        beginInfo.pInheritanceInfo = nullptr;
+
+        VkCommandBuffer cb = m_commandBuffer;
+        vkResetCommandBuffer(cb, VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT);
+        vkBeginCommandBuffer(cb, &beginInfo);
+
+        VK_CHECK(vkEndCommandBuffer(cb));
+
+        const std::vector<VkPipelineStageFlags> waitStages{VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+        const std::vector<uint64_t> signalValues{123};
+
+        VkTimelineSemaphoreSubmitInfo timelineInfo{};
+        timelineInfo.sType = VK_STRUCTURE_TYPE_TIMELINE_SEMAPHORE_SUBMIT_INFO;
+        timelineInfo.pNext = NULL;
+        timelineInfo.waitSemaphoreValueCount = 0;
+        timelineInfo.pWaitSemaphoreValues = NULL;
+        timelineInfo.signalSemaphoreValueCount = 1;
+        timelineInfo.pSignalSemaphoreValues = signalValues.data();
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.pNext = &timelineInfo;
+        submitInfo.waitSemaphoreCount = 0;
+        submitInfo.pWaitSemaphores = nullptr;
+        submitInfo.pWaitDstStageMask = waitStages.data();
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &m_commandBuffer;
+        submitInfo.signalSemaphoreCount = 1;
+        submitInfo.pSignalSemaphores = &m_timelineSemaphore;
+
+        VK_CHECK(vkQueueSubmit(m_graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE));
+    }
+
+    destroyResources();
+
+    return 0;
+}
